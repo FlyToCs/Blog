38178cdd33b72b8988b371621de5cb3fc1403e0d
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-11-22 15:16:49 +0330

    Add project files.

---
+++ src/Domain/Core/Blog.Domain.core/_common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Blog.Domain.core._common;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    
+}
+
---
+++ src/Presentation/Blog.Presentation.RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.Presentation.RazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
