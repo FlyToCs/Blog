52737a1a910ed944acd1702e9226dd0076fbcbd2
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-11-24 13:11:37 +0330

    add admin panel

---
+++ src/Presentation/Blog.Presentation.RazorPages/Areas/OperatorPanel/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.Presentation.RazorPages.Areas.OperatorPanel.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- src/Presentation/Blog.Presentation.RazorPages/Program.cs
+++ src/Presentation/Blog.Presentation.RazorPages/Program.cs
@@ -13,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
@@ -55,6 +56,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages()
    .WithStaticAssets();
 
